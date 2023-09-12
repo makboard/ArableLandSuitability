718813c8149d70b75790e1ef2cf0ef1563094783
Escape142 <escape756@gmail.com>
2023-09-12 15:41:51 +0000

    conv_lstm

--- src/model_utils.py
+++ src/model_utils.py
@@ -599,6 +599,7 @@ class Crop_LSTM(nn.Module):
     def forward(self, X):
         # input = X[0].permute(0, 2, 1) # (batch_size, seq_len, features)
         output, _ = self.lstm(X[0])
+        # extract only the last time step
         output = output[:, -1, :]
         output = self.net(torch.cat((output, X[1]), dim=1))
         return F.log_softmax(output, dim=1)
@@ -610,11 +611,195 @@ class CustomWeightedRandomSampler(WeightedRandomSampler):
 
     def __iter__(self):
         rand_tensor = np.random.choice(range(0, len(self.weights)),
-                                       size=self.num_samples,
-                                       p=self.weights.numpy() / torch.sum(self.weights).numpy(),
-                                       replace=self.replacement)
+                                    size=self.num_samples,
+                                    p=self.weights.numpy() / torch.sum(self.weights).numpy(),
+                                    replace=self.replacement)
         rand_tensor = torch.from_numpy(rand_tensor)
         return iter(rand_tensor.tolist())
+
+
+class ConvLSTMCell(nn.Module):
+    """
+    Initialize ConvLSTM cell.
+
+    Args:
+    input_dim (int): Number of channels of input tensor.
+    hidden_dim (int): Number of channels of hidden state.
+    kernel_size (int): Size of the convolutional kernel.
+    bias (bool): Whether to add the bias.
+    """
+    
+    def __init__(self,
+        input_dim,
+        hidden_dim,
+        kernel_size,
+        bias):
+
+        super(ConvLSTMCell, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.kernel_size = kernel_size
+        self.padding = kernel_size[0] // 2
+        self.bias = bias
+
+        self.conv = nn.Conv1d(in_channels=self.input_dim + self.hidden_dim,
+                            out_channels=4 * self.hidden_dim,
+                            kernel_size=self.kernel_size,
+                            padding=self.padding,
+                            bias=self.bias)
+
+    def forward(self, input, cur_state):
+        h_cur, c_cur = cur_state
+        combined = torch.cat([input, h_cur], dim=1)  # concatenate along channel axis
+
+        combined_conv = self.conv(combined)
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+
+        c_next = f * c_cur + i * g
+        h_next = o * torch.tanh(c_next)
+
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, length):
+        return (torch.zeros(batch_size, self.hidden_dim, length, device=self.conv.weight.device),
+                torch.zeros(batch_size, self.hidden_dim, length, device=self.conv.weight.device))
+
+
+class Crop_ConvLSTM(nn.Module):
+
+    """
+    A PyTorch module implementing a Crop Conv LSTM network.
+    
+    Parameters:
+        input_dim: Number of channels in input
+        hidden_dim: Number of hidden channels
+        kernel_size: Size of kernel in convolutions
+        n_layers: Number of LSTM layers stacked on each other
+        bias: Bias or no bias in Convolution
+        return_all_layers: Return the list of computations for all layers
+
+    Input:
+        A tensor of size B, T, C
+    Output:
+        A tuple of two lists of length n_layers (or length 1 if return_all_layers is False).
+            0 - layer_output_list is the list of lists of length T of each output
+            1 - last_state_list is the list of last states
+                    each element of the list is a tuple (h, c) for hidden state and memory
+    """
+
+    def __init__(self,
+        input_dim: int,
+        hidden_dim: int,
+        kernel_size: tuple,
+        n_layers: int,
+        n_classes: int,
+        input_len_monthly: int,
+        seq_len: int,
+        input_len_static: int,
+        bias: bool=True,
+        return_all_layers: bool=False
+        ) -> None:
+        super(Crop_ConvLSTM, self).__init__()
+
+        self._check_kernel_size_consistency(kernel_size)
+        # Make sure that both `kernel_size` and `hidden_dim` are lists having len == n_layers
+        kernel_size = self._extend_for_multilayer(kernel_size, n_layers)
+        hidden_dim = self._extend_for_multilayer(hidden_dim, n_layers)
+        if not len(kernel_size) == len(hidden_dim) == n_layers:
+            raise ValueError('Inconsistent list length.')
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.kernel_size = kernel_size
+        self.n_layers = n_layers
+        self.bias = bias
+        self.return_all_layers = return_all_layers
+        self.n_classes = n_classes
+        self.input_len_monthly = input_len_monthly
+        self.seq_len = seq_len
+        self.input_len_static = input_len_static
+        
+        cell_list = []
+        for i in range(0, self.n_layers):
+            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i - 1]
+
+            cell_list.append(ConvLSTMCell(input_dim=cur_input_dim,
+                                        hidden_dim=self.hidden_dim[i],
+                                        kernel_size=self.kernel_size[i],
+                                        bias=self.bias))
+        self.cell_list = nn.ModuleList(cell_list)
+        
+        self.flatten = nn.Flatten()
+        self.net = nn.Sequential(
+            nn.Linear(self.hidden_dim[0]*self.seq_len*self.input_len_monthly+self.input_len_static, self.n_classes),
+            nn.ReLU()
+        )
+    def forward(self, input):
+        """
+        Args:
+        input (tuple):  input[0] is a tensor of shape (batch_size, sequence_length, input_size) containing the monthly input sequence.
+                        input[1] is a tensor of shape (batch_size, input_size) containing the static input sequence.
+
+        Returns
+        -------
+        last_state_list, layer_output
+        """
+        input_monthly = input[0][:, None, :, :]  #fictional dimension added. will be used as channnels
+        input_static = input[1]
+        b = input_monthly.size()[0]
+
+        # Implement stateful ConvLSTM
+        hidden_state = self._init_hidden(batch_size=b, length=self.input_len_monthly)
+
+        layer_output_list = []
+        last_state_list = []
+        cur_layer_input = input_monthly
+
+        for layer_idx in range(self.n_layers):
+
+            h, c = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(self.seq_len):
+                h, c = self.cell_list[layer_idx](input = cur_layer_input[:, :, t, :],
+                                                cur_state=[h, c])
+                output_inner.append(h)
+
+            layer_output = torch.stack(output_inner, dim=1)
+            cur_layer_input = layer_output
+
+            layer_output_list.append(layer_output)
+            last_state_list.append([h, c])
+
+        if not self.return_all_layers:
+            layer_output_list = layer_output_list[-1:]
+            last_state_list = last_state_list[-1:]
+
+        output_monthly = self.flatten(layer_output_list[0])        
+        output = self.net(torch.cat((output_monthly, input_static), dim=1))
+
+        return nn.functional.log_softmax(output, dim=1)
+
+    def _init_hidden(self, batch_size, length):
+        init_states = []
+        for i in range(self.n_layers):
+            init_states.append(self.cell_list[i].init_hidden(batch_size, length))
+        return init_states
+
+    @staticmethod
+    def _check_kernel_size_consistency(kernel_size):
+        if not (isinstance(kernel_size, tuple) or
+                (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
+            raise ValueError('`kernel_size` must be tuple or list of tuples')
+
+    @staticmethod
+    def _extend_for_multilayer(param, n_layers):
+        if not isinstance(param, list):
+            param = [param] * n_layers
+        return param
 
 
 class CroplandDataModule_MLP(pl.LightningDataModule):
